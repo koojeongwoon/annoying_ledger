ee7703becd2041bca6b1760350d60e8106ced2e3
jwkoo <jwkoo@blackholic.com>
2025-09-26 13:40:35 +0900

    자바 버전 업데이트

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,8 @@
+import com.android.build.gradle.BaseExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.tasks.compile.JavaCompile
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 allprojects {
     repositories {
         google()
@@ -17,6 +22,24 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    plugins.withId("com.android.library") {
+        extensions.findByType(BaseExtension::class.java)?.compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_21
+            targetCompatibility = JavaVersion.VERSION_21
+        }
+    }
+
+    tasks.withType(JavaCompile::class.java).configureEach {
+        sourceCompatibility = JavaVersion.VERSION_21.toString()
+        targetCompatibility = JavaVersion.VERSION_21.toString()
+    }
+
+    tasks.withType(KotlinCompile::class.java).configureEach {
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_21.toString()
+    }
 }
 
 tasks.register<Delete>("clean") {
